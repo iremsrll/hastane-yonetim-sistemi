88dd5f6d6c684b4a2a22bd28dcde70d906004db0
iremsrll <iremsaral2013@gmail.com>
2025-12-30 17:32:32 +0300

    doctors ile ilgili dosya ve entry pointsler ekle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientsModule } from './patients/patients.module';
+import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
-  imports: [],
+  imports: [PatientsModule, DoctorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
