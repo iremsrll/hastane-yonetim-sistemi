8d7bbf2a7a2c5334fe4e4cbd0183db4bbfbaf81d
iremsrll <iremsaral2013@gmail.com>
2025-12-30 17:49:37 +0300

    app.module güncellendi ve tsconfig bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PatientsModule } from './patients/patients.module';
 import { DoctorsModule } from './doctors/doctors.module';
 import { DepartmentsModule } from './departments/departments.module';
@@ -8,8 +8,31 @@ import { AppointmentsModule } from './appointments/appointments.module';
 import { PrescriptionsModule } from './prescriptions/prescriptions.module';
 
 @Module({
-  imports: [PatientsModule, DoctorsModule, DepartmentsModule, AppointmentsModule, PrescriptionsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get<string>('DB_HOST'),
+        port: 1433, 
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true, 
+        synchronize: true, 
+        options: {
+          encrypt: false,
+          trustServerCertificate: true,
+        },
+      }),
+    }),
+    PatientsModule,
+    DoctorsModule,
+    DepartmentsModule,
+    AppointmentsModule,
+    PrescriptionsModule,
+  ],
 })
 export class AppModule {}
