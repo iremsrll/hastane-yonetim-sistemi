1a81b63bdfc06052dae5f35ebec0d71702c93c29
iremsrll <iremsaral2013@gmail.com>
2026-01-05 11:18:12 +0300

    Swagger yapılandırması main.ts güncellemesi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Hastane Yönetim Sistemi API')
+    .setDescription('Sağlık personeli ve hasta randevu yönetim sistemi')
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); 
+
+  await app.listen(3000);
 }
 bootstrap();
