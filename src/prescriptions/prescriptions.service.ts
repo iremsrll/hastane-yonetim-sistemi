3ab20271b61a8cb7b1a74caf2a303eb9bc4d4388
iremsrll <iremsaral2013@gmail.com>
2026-01-05 11:13:28 +0300

    reçete servisi ve tüm modüllerin CRUD güncelleme

--- src/prescriptions/prescriptions.service.ts
+++ src/prescriptions/prescriptions.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Prescription } from './entities/prescription.entity';
 import { CreatePrescriptionDto } from './dto/create-prescription.dto';
-import { UpdatePrescriptionDto } from './dto/update-prescription.dto';
 
 @Injectable()
 export class PrescriptionsService {
-  create(createPrescriptionDto: CreatePrescriptionDto) {
-    return 'This action adds a new prescription';
+  constructor(
+    @InjectRepository(Prescription)
+    private readonly prescRepo: Repository<Prescription>,
+  ) {}
+
+  async create(dto: CreatePrescriptionDto) {
+    const prescription = this.prescRepo.create({
+      medications: dto.medications,
+      notes: dto.notes,
+      appointment: { appoId: dto.appointmentId } as any,
+    });
+    return await this.prescRepo.save(prescription);
   }
 
-  findAll() {
-    return `This action returns all prescriptions`;
+  async findAll() {
+    return await this.prescRepo.find({ relations: ['appointment'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} prescription`;
+  async findOne(id: number) {
+    const presc = await this.prescRepo.findOne({
+      where: { prescId: id },
+      relations: ['appointment'],
+    });
+    if (!presc) throw new NotFoundException(`Reçete kaydı (#${id}) bulunamadı.`);
+    return presc;
   }
 
-  update(id: number, updatePrescriptionDto: UpdatePrescriptionDto) {
-    return `This action updates a #${id} prescription`;
+  async update(id: number, dto: any) {
+    const presc = await this.findOne(id);
+    return await this.prescRepo.save({ ...presc, ...dto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} prescription`;
+  async remove(id: number) {
+    const presc = await this.findOne(id);
+    return await this.prescRepo.remove(presc);
   }
 }
